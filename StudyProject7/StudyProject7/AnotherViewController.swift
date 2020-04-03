b77ee5ddeaafcbba619e4055b8ae7a116546ad74
Nikolai Faustov <61990876+nfaustov@users.noreply.github.com>
2020-04-03 18:17:19 +0300

    changes added

---
+++ StudyProject7/StudyProject7/AnotherViewController.swift
@@ -0,0 +1,18 @@
+//
+//  AnotherViewController.swift
+//  StudyProject7
+//
+//  Created by Nikolai Faustov on 31.03.2020.
+//  Copyright © 2020 Nikolai Faustov. All rights reserved.
+//
+
+import UIKit
+
+class AnotherViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
---
+++ StudyProject7/StudyProject7/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StudyProject7
+//
+//  Created by Nikolai Faustov on 31.03.2020.
+//  Copyright © 2020 Nikolai Faustov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+}
+
