072d758fbbabbcc0783d0b62050eb0b68ff187bf
Nikolai Faustov <61990876+nfaustov@users.noreply.github.com>
2020-04-12 21:56:10 +0300

    bad constant added

---
+++ Bad Constant/Bad Constant/FoxMoneyViewController.swift
@@ -0,0 +1,18 @@
+//
+//  FoxMoneyViewController.swift
+//  Bad Constant
+//
+//  Created by Nikolai Faustov on 08.04.2020.
+//  Copyright © 2020 Nikolai Faustov. All rights reserved.
+//
+
+import UIKit
+
+class FoxMoneyViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
