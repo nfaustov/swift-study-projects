05be4dfa8a42e83f5bc6be2114519763345560d1
Nikolai Faustov <61990876+nfaustov@users.noreply.github.com>
2020-11-22 20:28:37 +0300

    homework12 init

---
+++ StudyProject12/StudyProject12/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StudyProject12
+//
+//  Created by Nikolai Faustov on 01.10.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
