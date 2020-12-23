abbd5e9f931766af266c0c2714c322665e8c4672
Nikolai Faustov <61990876+nfaustov@users.noreply.github.com>
2020-12-23 17:18:25 +0300

    homework12 updated

---
+++ StudyProject12/StudyProject12/WeatherServices/CodableWeatherDecoder.swift
@@ -0,0 +1,8 @@
+//
+//  CodableWeatherDecoder.swift
+//  StudyProject12
+//
+//  Created by Nikolai Faustov on 22.12.2020.
+//
+
+import Foundation
---
+++ StudyProject12/StudyProject12/WeatherServices/HandWeatherDecoder.swift
@@ -0,0 +1,8 @@
+//
+//  HandWeatherDecoder.swift
+//  StudyProject12
+//
+//  Created by Nikolai Faustov on 22.12.2020.
+//
+
+import Foundation
