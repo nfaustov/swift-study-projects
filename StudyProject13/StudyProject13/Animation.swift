eb7e459613fbb016703958676995c4f0e328684d
Nikolai Faustov <61990876+nfaustov@users.noreply.github.com>
2021-01-21 16:32:32 +0300

    homework13 updated

--- StudyProject13/StudyProject13/Animation.swift
+++ StudyProject13/StudyProject13/Animation.swift
@@ -14,43 +14,43 @@ struct Animation {
 
 extension Animation {
     static func fadeOut(duration: TimeInterval) -> Animation {
-        return Animation(duration: duration, handler: { $0.alpha = 0 })
+        return Animation(duration: duration) { $0.alpha = 0 }
     }
     
     static func fadeIn(duration: TimeInterval) -> Animation {
-        return Animation(duration: duration, handler: { $0.alpha = 1 })
+        return Animation(duration: duration) { $0.alpha = 1 }
     }
     
     static func paintYellow(duration: TimeInterval) -> Animation {
-        return Animation(duration: duration, handler: { $0.backgroundColor = .systemYellow })
+        return Animation(duration: duration) { $0.backgroundColor = .systemYellow }
     }
     
     static func paintRed(duration: TimeInterval) -> Animation {
-        return Animation(duration: duration, handler: { $0.backgroundColor = .systemRed })
+        return Animation(duration: duration) { $0.backgroundColor = .systemRed }
     }
     
     static func translation(duration: TimeInterval) -> Animation {
-        return Animation(duration: duration, handler: { $0.transform = .init(translationX: 100, y: -100) })
+        return Animation(duration: duration) { $0.transform = .init(translationX: 100, y: -100) }
     }
     
     static func rotate(duration: TimeInterval) -> Animation {
-        return Animation(duration: duration, handler: { $0.transform = .init(rotationAngle: CGFloat.pi) })
+        return Animation(duration: duration) { $0.transform = .init(rotationAngle: CGFloat.pi) }
     }
     
     static func scaleUp(duration: TimeInterval) -> Animation {
-        return Animation(duration: duration, handler: { $0.transform = .init(scaleX: 2, y: 2) })
+        return Animation(duration: duration) { $0.transform = .init(scaleX: 2, y: 2) }
     }
     
     static func identityTransform(duration: TimeInterval) -> Animation {
-        return Animation(duration: duration, handler: { $0.transform =  .identity })
+        return Animation(duration: duration) { $0.transform =  .identity }
     }
     
     static func makeItRound(duration: TimeInterval) -> Animation {
-        return Animation(duration: duration, handler: { $0.layer.cornerRadius = 75 })
+        return Animation(duration: duration) { $0.layer.cornerRadius = 75 }
     }
     
     static func makeItSquare(duration: TimeInterval) -> Animation {
-        return Animation(duration: duration, handler: { $0.layer.cornerRadius = 0 })
+        return Animation(duration: duration) { $0.layer.cornerRadius = 0 }
     }
 }
 
@@ -65,6 +65,21 @@ extension UIView {
             animation.handler(self)
         } completion: { _ in
             self.animate(animations, options: options)
+        }
+    }
+}
+// отличие в одну строчку, поэтому не стал использовать
+extension UIView {
+    func runningPropertyAnimator(_ animations: [Animation], options: UIView.AnimationOptions) {
+        guard !animations.isEmpty else { return }
+        
+        var animations = animations
+        let animation = animations.removeFirst()
+        
+        UIViewPropertyAnimator.runningPropertyAnimator(withDuration: animation.duration, delay: 0, options: options) {
+            animation.handler(self)
+        } completion: { _ in
+            self.runningPropertyAnimator(animations, options: options)
         }
     }
 }
--- StudyProject13/StudyProject13/ViewController.swift
+++ StudyProject13/StudyProject13/ViewController.swift
@@ -13,6 +13,13 @@ final class ViewController: UIViewController {
     
     private var redBox: UIView!
     
+    var animator: UIViewPropertyAnimator!
+    
+    let reversedAnimator = UIViewPropertyAnimator(duration: 1, curve: .linear)
+    let repeatAnimator = UIViewPropertyAnimator(duration: 1, curve: .linear)
+    
+    var rotationAngle = CGFloat.zero
+    
     private var animationNumber = 0 {
         didSet {
             descriptionLabel.text = "\(animationNumber)"
@@ -26,9 +33,7 @@ final class ViewController: UIViewController {
         
         redBox = UIView()
         redBox.backgroundColor = .systemRed
-        
-        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(handleTapGesture(_:)))
-        redBox.addGestureRecognizer(tapGesture)
+
         view.addSubview(redBox)
         
         redBox.translatesAutoresizingMaskIntoConstraints = false 
@@ -38,32 +43,89 @@ final class ViewController: UIViewController {
             redBox.widthAnchor.constraint(equalToConstant: 150),
             redBox.heightAnchor.constraint(equalToConstant: 150)
         ])
+        
+        launchAnimation()
     }
 
     @IBAction func previousAnimation() {
+        if animator.state == .active {
+            animator.stopAnimation(false)
+        }
+        if animator.state == .stopped {
+            animator.finishAnimation(at: .start)
+        }
         animationNumber = max(1, animationNumber - 1)
+        launchAnimation()
     }
     
     @IBAction func nextAnimation() {
+        if animator.state == .active {
+            animator.stopAnimation(false)
+        }
+        if animator.state == .stopped {
+            animator.finishAnimation(at: .start)
+        }
         animationNumber = min(7, animationNumber + 1)
+        launchAnimation()
     }
     
-    @objc func handleTapGesture(_ recognizer: UITapGestureRecognizer) {
-        let options: UIView.AnimationOptions = animationNumber == 7 ? [.repeat] : []
-        redBox.animate(animations(), options: options)
+    private func launchAnimation() {
+        //let options: UIView.AnimationOptions = animationNumber == 7 ? [.repeat] : []
+        //redBox.animate(animations(), options: options)
+        animator = UIViewPropertyAnimator(duration: 1, curve: .linear)
+        animator.addAnimations {
+            self.animation()
+        }
+        
+        animator.addCompletion { _ in
+            if self.animationNumber == 7 {
+                self.rotationAngle += CGFloat.pi
+                self.launchAnimation()
+            } else {
+                self.reversedAnimator.addAnimations(self.reversedAnimation)
+                self.reversedAnimator.startAnimation()
+            }
+        }
+        
+        animator.startAnimation()
     }
-
-    private func animations() -> [Animation] {
-        switch animationNumber {
-        case 1: return [.paintYellow(duration: 1), .paintRed(duration: 1)]
-        case 2: return [.translation(duration: 1), .identityTransform(duration: 1)]
-        case 3: return [.makeItRound(duration: 1), .makeItSquare(duration: 1)]
-        case 4: return [.rotate(duration: 1), .identityTransform(duration: 1)]
-        case 5: return [.fadeOut(duration: 1), .fadeIn(duration: 1)]
-        case 6: return [.scaleUp(duration: 1), .identityTransform(duration: 1)]
-        case 7: return [.rotate(duration: 2)]
-        default: return []
+    
+    private func animation() {
+        switch animationNumber{
+        case 1: redBox.backgroundColor = .systemYellow
+        case 2: redBox.transform = .init(translationX: 100, y: -100)
+        case 3: redBox.layer.cornerRadius = 75
+        case 4: redBox.transform = .init(rotationAngle: CGFloat.pi)
+        case 5: redBox.alpha = 0
+        case 6: redBox.transform = .init(scaleX: 2, y: 2)
+        case 7: redBox.transform = .init(rotationAngle: rotationAngle)
+        default: break
         }
     }
+    
+    private func reversedAnimation() {
+        switch animationNumber{
+        case 1: redBox.backgroundColor = .systemRed
+        case 2: redBox.transform = .identity
+        case 3: redBox.layer.cornerRadius = 0
+        case 4: redBox.transform = .identity
+        case 5: redBox.alpha = 1
+        case 6: redBox.transform = .identity
+        default: break
+        }
+    }
+
+//    private func animations() -> [Animation] {
+//        switch animationNumber {
+//        case 1: return [.paintYellow(duration: 1), .paintRed(duration: 1)]
+//        case 2: return [.translation(duration: 1), .identityTransform(duration: 1)]
+//        case 3: return [.makeItRound(duration: 1), .makeItSquare(duration: 1)]
+//        case 4: return [.rotate(duration: 1), .identityTransform(duration: 1)]
+//        case 5: return [.fadeOut(duration: 1), .fadeIn(duration: 1)]
+//        case 6: return [.scaleUp(duration: 1), .identityTransform(duration: 1)]
+//        case 7: return [.rotate(duration: 2)]
+//        default: return []
+//        }
+//    }
 }
 
